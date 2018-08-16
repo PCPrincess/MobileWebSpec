ef7aa8e07d45fc9b7e53cd92f19c3aa46a6c9cd6
PCPrincess <PCPrincess@users.noreply.github.com>
2018-08-16 13:47:20 -0700

    Much Work Done - Up to and Including AJAX w/Fetch

--- Section-1/Lesson11/lesson5-semantics-aria/21-dialog/modal.js
+++ Section-1/Lesson11/lesson5-semantics-aria/21-dialog/modal.js
@@ -64,6 +64,9 @@ function openModal(e) {
   }
 
   // FIXME: hide non-modal content from screen readers
+  document.querySelector('.wrapper').setAttribute('aria-hidden', true);
+  // Note: Everything NOT in the modal is in a div container with
+  //       a class="wrapper"
 }
 
 function closeModal() {
@@ -75,4 +78,6 @@ function closeModal() {
   focusedElementBeforeModal.focus();
 
   // FIXME: don't forget to make main content screen reader accessible again.
+  document.querySelector('.wrapper').removeAttribute('aria-hidden');
+  // Note: Okay, we'll just remove the attribute!
 }
